07836b8933ccb20679525e5ee775c11150d8c75f
Alex Canez <el.winy.36@gmail.com>
2022-07-10 12:28:55 -0600

    modified server

--- server.js
+++ server.js
@@ -1,20 +1,31 @@
 const express = require('express');
 const { User } = require('./models');
-const db = require('./config/connection')
-//const routes = require('./controllers')
+const db = require('./config/connection');
+const routes = require('./controllers');
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-//app.use(routes)
+app.use(require('./routes'));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api-ac', {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+mongoose.set('debug', true);
 
 db.once('open', ()=>{
     app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
   });
-})
+});
+
+
 
 
 
