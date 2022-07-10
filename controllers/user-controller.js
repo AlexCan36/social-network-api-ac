62a70f69b2b75c3e97262528cd7b2942011342f8
Alex Canez <el.winy.36@gmail.com>
2022-07-10 11:21:10 -0600

    added user controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,1 +1,68 @@
 const { User } = require('../models');
+
+const userController = {
+    getUsers(req, res) {
+        User.find({})
+        .then(userData => res.json(userData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    },
+
+    addUser({ body } , res) {
+        User.create(body)
+        .then(userData => res.json(userData))
+        .catch(err => res.status(400).json(err));
+    },
+
+    getUserById({ params }, res) {
+        User.findOne({ _id: params.id })
+        .then(userData => res.json(userData))
+        .catch(err => res.status(400).json(err));
+    },
+
+    updateUser({ params }, res) {
+        User.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+        .then(userData => {
+            if(!userData) {
+                res.status(404).json({ message: 'no user found with id' });
+                return;
+            }
+            res.json(userData);
+        })
+        .catch(err => res.status(400));
+    },
+
+    deleteUser({ params }, res) {
+        User.findByIdAndDelete({ _id: params.id })
+        .then(userData => {
+            if(!userData) {
+                res.status(404).json({ message: 'no user found with id' });
+                return;
+            }
+            res.json(userData);
+        })
+        .catch(err => res.status(404).json(err));
+    },
+
+    addFriend ({ params }, res) {
+        User.findByIdAndUpdate({ _id: params.id },
+        { $push: {friends: params.friendId }},
+        { runValidators: true, new: true})
+        .then(userData => {
+            if(!userData) {
+                res.status(404).json({ message: 'no user found with id' });
+                return;
+            }
+            res.json(userData);
+        })
+        .catch(err => res.status(404).json(err));
+    },
+    deleteFriend({params}, res) {
+
+    }
+
+}
+
+module.exports = userController;
